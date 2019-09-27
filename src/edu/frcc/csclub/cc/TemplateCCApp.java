2f45b81555332525de867a11f54f1f34d87f45cc
bandguru2007 <55898891+bandguru2007@users.noreply.github.com>
2019-09-27 16:26:30 -0600

    Update TemplateCCApp.java
    randomly move points

--- src/edu/frcc/csclub/cc/TemplateCCApp.java
+++ src/edu/frcc/csclub/cc/TemplateCCApp.java
@@ -22,6 +22,11 @@ public class TemplateCCApp extends CCApp {
 		xPoints = new int[NUM_POINTS];
 		yPoints = new int[NUM_POINTS];
 
+		for (int i = 0; i < NUM_POINTS; ++i) {
+			xPoints[i] = getRandom().nextInt(WIDTH);
+			yPoints[i] = getRandom().nextInt(HEIGHT);
+		}
+
 		getGfx().setColor(Color.blue);
 		getGfx().fillRect(0, 0, WIDTH, HEIGHT);
 	}
@@ -30,6 +35,10 @@ public class TemplateCCApp extends CCApp {
 	public void update() {
 		// TODO Update your application here.
 
+		for (int i = 0; i < NUM_POINTS; ++i) {
+			xPoints[i] += getRandom().nextInt(5) - 2;
+			yPoints[i] += getRandom().nextInt(5) - 2;
+		}
 	}
 
 	@Override
